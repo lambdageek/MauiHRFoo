c5a78bcff4b961c7bf898bfda869e93da64e6d91
Aleksey Kliger <alklig@microsoft.com>
2021-08-04 15:20:37 -0400

    Add project files.

---
+++ Foo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using System.Runtime.InteropServices;
+
+namespace Foo
+{
+	public partial class MainPage : ContentPage
+	{
+
+		[DllImport("FooBar", EntryPoint = "Nonsense")]
+		public static extern void Nonsense();
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		int count = 0;
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			OutMethod();
+			//Nonsense();
+		}
+
+		public void OutMethod()
+        {
+			count--;
+			CounterLabel.Text = $"{GetEE()}: {count}";
+		}
+
+		public string GetEE()
+		{
+			if (!System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported)
+				return "AOT";
+			if (!System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeCompiled)
+				return "Interp";
+			return "JIT";
+		}
+	}
+}
---
+++ Foo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace Foo
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
